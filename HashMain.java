b925a395f85500cbb0e657b5dc44ffecc16cc240
Devin Brown <devin.g.brown@gmail.com>
2015-02-21 15:44:04 -0700

    changes

---
+++ HashMain.java
@@ -0,0 +1,8 @@
+public class HashMain {
+  public static void main(String[] args) {
+    HashMap hashMap = new HashMap();
+    hashMap.put(1, 200);
+    System.out.println(hashMap.get(1));
+  }
+}
+
